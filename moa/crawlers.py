d963819c331824b4d5b2f188a45efacea3fdcf3f
pita <hjkim3323@gmail.com>
2017-12-20 11:11:30 +0900

    #9 reduce codes for crawling

--- moa/crawlers.py
+++ moa/crawlers.py
@@ -15,49 +15,28 @@ yes24_base_url = 'http://www.yes24.com/Mall/buyback/Search' \
 def yes24_crawl(soup, query, num_pages):
     """Crawl until the end of list in yes24"""
     yes24_resultset = []
-    items_per_page = 20
     cur_page = 1
     while cur_page <= num_pages:
-        try:
-            for i in range(1,items_per_page+1):
-                image = soup.select(
-                    'ul.clearfix li:nth-of-type(' + str(i) + ') div.bbG_img img'
-                    )
-                image_v = image[0].get('src')
-                title = soup.select(
-                    'ul.clearfix li:nth-of-type(' + str(i) + ') div.bbG_info a'
-                    )
-                title_v = title[0].get('title')
-                author = soup.select(
-                    'div.bbGoodsList > ul > li:nth-of-type(' + str(i) + ') span.bbG_auth'
-                    )
-                author_v = author[0].text
-                publisher = soup.select(
-                    'div.bbGoodsList > ul > li:nth-of-type(' + str(i) + ') span.bbG_pub'
-                    )
-                publisher_v = publisher[0].text
-                pubdate = soup.select(
-                    'div.bbGoodsList > ul > li:nth-of-type(' + str(i) + ') span.bbG_date'
-                    )
-                pubdate_v = pubdate[0].text
-                prices = soup.select(
-                    'ul.clearfix li:nth-of-type(' +str(i) + ') div.bbG_price td'
-                    )
-                # prices_v 인덱스: 0 정가, 1 바이백 최상 , 2 바이백 상, 3 바애빅 중
-                prices_v = list(map(lambda p:p.text, prices))
+        for tag in soup.select(".bbGoodsList ul.clearfix li"):
+            image_v = tag.select('div.bbG_img img')[0].get('src')
+            title_v = tag.select('div.bbG_info a')[0].get('title')
+            author_v = tag.select('span.bbG_auth')[0].text
+            publisher_v = tag.select('span.bbG_pub')[0].text
+            pubdate_v = tag.select('span.bbG_date')[0].text
+            prices = tag.select('div.bbG_price td')
+            # prices_v 인덱스: 0 정가, 1 바이백 최상 , 2 바이백 상, 3 바애빅 중
+            prices_v = list(map(lambda p:p.text, prices))
 
-                yes24_resultset.append({
-                    'image':image_v,
-                    'title':title_v,
-                    'author':author_v,
-                    'publisher':publisher_v,
-                    'pubdate':pubdate_v,
-                    'prices':prices_v,
-                    'platform':'yes24',
-                    'page':cur_page,
-                    })
-        except IndexError:
-            print("인덱스 에러!")
+            yes24_resultset.append({
+                'image':image_v,
+                'title':title_v,
+                'author':author_v,
+                'publisher':publisher_v,
+                'pubdate':pubdate_v,
+                'prices':prices_v,
+                'platform':'yes24',
+                'page':cur_page,
+                })
         cur_page += 1
         soup = soupify('yes24', yes24_base_url, query, cur_page)
     return yes24_resultset
@@ -93,36 +72,31 @@ aladin_base_url = 'http://used.aladin.co.kr/shop/usedshop/wc2b_search.aspx' \
 def aladin_crawl(soup, query, num_pages):
     """Crawl until the end of list in aladin"""
     aladin_resultset = []
-    items_per_page = 10
     cur_page = 1
     while cur_page <= num_pages:
-        try:
-            prices = soup.select('#searchResult .c2b_tablet3')
-            for i in range(items_per_page):
-                image = soup.select('#searchResult td img')
-                image_v = image[i].get('src')
-                title = soup.select('#searchResult td > a > strong')
-                title_v = title[i].text
-                # author, publisher, pubdate는 한번에 처리
-                auth_and_pub = soup.select('#searchResult tr:nth-of-type(' + str(8*i+2) +') br')[0].find_all(text=True)
-                str_join = ''.join(auth_and_pub)
-                author_v = str_join[:str_join.find("|")-1]
-                publisher_v = auth_and_pub[-3]
-                pubdate_v = auth_and_pub[-2].lstrip(" | ")
-                # prices_v 인덱스: 0 정가, 1 바이백 최상 , 2 바이백 상, 3 바애빅 중
-                prices_v = list(map(lambda p:p.text, prices[4*i:4*(i+1)]))
-                aladin_resultset.append({
-                    'image':image_v,
-                    'title':title_v,
-                    'author':author_v,
-                    'publisher':publisher_v,
-                    'pubdate':pubdate_v,
-                    'prices':prices_v,
-                    'platform':'aladin',
-                    'page':cur_page,
-                    })
-        except IndexError:
-            print("인덱스 에러!")
+        items = soup.select('#searchResult > tr')
+        tags = (t for t in items if items.index(t)%2==0)
+        for tag in tags:
+            image_v = tag.select('td img')[0].get('src')
+            title_v = tag.select('td > a > strong')[0].text
+            # author, publisher, pubdate는 한번에 처리
+            pubinfo = tag.select('td')[3].find_all(text=True)[:-1]
+            pubinfo = pubinfo[3:] if pubinfo[2].startswith(' -') else pubinfo[2:]
+            *author, publisher_v, pubdate = pubinfo
+            author_v = ''.join(author).rstrip(' | ')
+            pubdate_v = pubdate.lstrip(" | ")
+            # prices_v 인덱스: 0 정가, 1 바이백 최상 , 2 바이백 상, 3 바애빅 중
+            prices_v = list(p.text for p in tag.select('.c2b_tablet3'))
+            aladin_resultset.append({
+                'image':image_v,
+                'title':title_v,
+                'author':author_v,
+                'publisher':publisher_v,
+                'pubdate':pubdate_v,
+                'prices':prices_v,
+                'platform':'aladin',
+                'page':cur_page,
+                })
         cur_page += 1
         soup = soupify('aladin', aladin_base_url, query, cur_page)
     return aladin_resultset
