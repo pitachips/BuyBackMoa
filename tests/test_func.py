9fef2814c1e80f48aab73da5223b4b1e56edc7f7
pita <hjkim3323@gmail.com>
2018-05-17 21:22:15 +0900

    #45 TravisCI 4

--- tests/test_func.py
+++ tests/test_func.py
@@ -94,6 +94,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser.quit()
         # 사파리를 열고, '고성능 파이썬'을 검색한다.
         self.browser = webdriver.Chrome(chrome_options=options)
+        self.browser.implicitly_wait(10)
         self.browser.get(self.live_server_url)
         # 아주 빠른 시간 내에 리턴되어야 한다.
         self.send_searchword_to_searchbox('고성능 파이썬', 1)
